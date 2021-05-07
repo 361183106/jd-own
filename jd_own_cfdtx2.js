e5f8647a14134d97347f841dbf39b68df2f50ce0
李聪 <296114711@qq.com>
2021-05-07 13:39:47 +0800

    新增定时任务

--- jd_own_cfdtx2.js
+++ jd_own_cfdtx2.js
@@ -17,8 +17,8 @@
 
  ============QuantumultX==============
  [task_local]
- #京豆变动通知
- 0 0 * * * https://gitee.com/Misyi/jd-own/raw/master/jd_own_cfdtx2.js, tag=财富岛提现, img-url=https://raw.githubusercontent.com/Orz-3/task/master/jd.png, enabled=true
+ #财富岛提现
+ 0-5 0 * * * https://gitee.com/Misyi/jd-own/raw/master/jd_own_cfdtx2.js, tag=财富岛提现, img-url=https://raw.githubusercontent.com/Orz-3/task/master/jd.png, enabled=true
 
   # quanx
   [rewrite_local]
