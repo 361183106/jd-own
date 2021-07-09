8367fff2bf87104b103f2d8d1760911c619e14a7
haowei.deng <haowei.deng@huolala.cn>
2021-07-09 16:41:13 +0800

    通知

--- wool/wx_crackstep.js
+++ wool/wx_crackstep.js
@@ -65,7 +65,8 @@ function updateStep() {
                     if (data) {
                         data = JSON.parse(data);
                         console.log(`${JSON.stringify(data)}`)
-                        notify.sendNotify(phone+`修改微信步数成功 - 步数为： `+step);
+                        let s = phone+'修改微信步数成功 - 步数为： '+step;
+                        notify.sendNotify(s);
                     } else {
                         console.log(`服务器返回空数据`)
                     }
